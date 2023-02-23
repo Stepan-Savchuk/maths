68bf20e598223badf30acd59dc38aec91cacd493
Stepan-Savchuk <stepan_savchuk3@protonmail.com>
2023-02-23 11:28:20 +0200

    I have made input of numbers

--- percent.c
+++ percent.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(){
-	int number = 60943;
+	int number;
+	int perc;
+	printf("Please enter number from which you want to find percentage");
+	scanf("%d", &number);
+	printf("Please enter percentage of given number you want to find");
+	scanf("%d", &perc);
+	printf("%d %d", number, perc);
 	return 0;
 }
