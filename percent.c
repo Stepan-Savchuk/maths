cc1a742d6e64da0415cdcb9433bd7f3d2eacc0a9
Stepan-Savchuk <stepan_savchuk3@protonmail.com>
2023-02-24 07:29:43 +0200

    Implemented finding of percentage of given number

--- percent.c
+++ percent.c
@@ -3,10 +3,11 @@
 int main(){
 	int number;
 	int perc;
-	printf("Please enter number from which you want to find percentage");
+	printf("Please enter number from which you want to find percentage ");
 	scanf("%d", &number);
-	printf("Please enter percentage of given number you want to find");
+	printf("\nPlease enter percentage of given number you want to find ");
 	scanf("%d", &perc);
-	printf("%d %d", number, perc);
+	double result = ((number / 100.0) * (float) perc);
+	printf("Result %f", result);
 	return 0;
 }
