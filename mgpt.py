25eab9a53030bbe25894eeaa311fb85f5ae8eccd
Stepan-Savchuk <stepan_savchuk3@protonmail.com>
2023-03-06 20:45:46 +0200

    I have added python code which misha generated with chatgpt

---
+++ mgpt.py
@@ -0,0 +1,149 @@
+import json
+
+class Dictionary:
+    def __init__(self, language):
+        self.language = language
+        self.words = {}
+
+    def add_word(self, word, definitions):
+        if word in self.words:
+            self.words[word].extend(definitions)
+        else:
+            self.words[word] = definitions
+
+    def update_word(self, word, definitions):
+        self.words[word] = definitions
+
+    def delete_word(self, word):
+        if word in self.words:
+            del self.words[word]
+
+    def search_word(self, word):
+        if word in self.words:
+            return self.words[word]
+        else:
+            return None
+
+    def save(self, dictionary):
+        with open(dictionary, "w") as f:
+            json.dump(self.__dict__, f)
+
+
+class DictionaryApp:
+    def __init__(self):
+        self.dictionaries = {}
+
+    def create_dictionary(self, language):
+        self.dictionaries[language] = Dictionary(language)
+
+    def add_word(self, language, word, definitions):
+        if language in self.dictionaries:
+            self.dictionaries[language].add_word(word, definitions)
+        else:
+            print("Dictionary not found")
+
+    def update_word(self, language, word, definitions):
+        if language in self.dictionaries:
+            self.dictionaries[language].update_word(word, definitions)
+        else:
+            print("Dictionary not found")
+
+    def delete_word(self, language, word):
+        if language in self.dictionaries:
+            self.dictionaries[language].delete_word(word)
+        else:
+            print("Dictionary not found")
+
+    def search_word(self, language, word):
+        if language in self.dictionaries:
+            definitions = self.dictionaries[language].search_word(word)
+            if definitions:
+                print(f"{word}: {definitions}")
+            else:
+                print(f"{word} not found")
+        else:
+            print("Dictionary not found")
+
+    def save_dictionary(self, language, dictionary):
+        if language in self.dictionaries:
+            self.dictionaries[language].save(dictionary)
+        else:
+            print("Dictionary not found")
+
+    def load_dictionary(self, dictionary):
+        with open(dictionary, "r") as f:
+            data = json.load(f)
+        language = data['language']
+        dictionary = Dictionary(language)
+        dictionary.words = data['words']
+        self.dictionaries[language] = dictionary
+
+    def print_menu(self):
+        print("""
+        1. Create dictionary
+        2. Add word
+        3. Update word
+        4. Delete word
+        5. Search word
+        6. Save dictionary
+        7. Quit
+        """)
+
+    def run(self):
+        while True:
+            self.print_menu()
+            choice = input("Enter your choice: ")
+
+            if choice == "1":
+                language = input("Enter language: ")
+                self.create_dictionary(language)
+            elif choice == "2":
+                language = input("Enter language: ")
+                word = input("Enter word: ")
+                definitions = input("Enter definitions separated by commas: ").split(",")
+                self.add_word(language, word, definitions)
+            elif choice == "3":
+                language = input("Enter language: ")
+                word = input("Enter word: ")
+                definitions = input("Enter definitions separated by commas: ").split(",")
+                self.update_word(language, word, definitions)
+            elif choice == "4":
+                language = input(("Enter language: "))
+                word = input('Enter word you want to delete: ')
+                self.dictionaries[language].delete_word(word)
+            elif choice == '5':
+                language = input(("Enter language: "))
+                word = input('Enter the word, the interpretation of which you need to find: ')
+                definitions = self.dictionaries[language].search_word(word)
+                meanings = definitions.find_meanings(word)
+                if meanings:
+                    print(f'Interpretation of the word "{word}":')
+                    for i, meaning in enumerate(meanings):
+                        print(f'{i + 1}. {meaning}')
+                else:
+                    print(f'word  "{word}" nor found.')
+            elif choice == "6":
+                language = input(("Enter language: "))
+                word = input("Enter the word you want to export: ")
+                meanings = self.dictionaries[language].get(word)
+                if meanings:
+                    dictionary = f"{word}_meanings.txt"
+                    with open(dictionary, "w") as file:
+                        for meaning in meanings:
+                            file.write(meaning + "\n")
+                            print(f"word '{word}' and its interpretation were successfully exported to a file '{dictionary}'")
+                        else:
+                            print(f"word '{word}' not found.")
+
+            elif choice == "7":
+                print("Close program")
+            break
+
+
+
+
+
+
+
+
+
