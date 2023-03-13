faa9c8771a53244c81df39144298144d6cd5b9e8
Stepan-Savchuk <stepan_savchuk@protonmail.com>
2023-03-13 10:49:54 +0000

    I have improved percentagea program

--- percent.c
+++ percent.c
@@ -3,11 +3,21 @@
 int main(){
 	int number;
 	int perc;
+
 	printf("Please enter number from which you want to find percentage ");
 	scanf("%d", &number);
 	printf("\nPlease enter percentage of given number you want to find ");
 	scanf("%d", &perc);
+
 	double result = ((number / 100.0) * (float) perc);
-	printf("Result %f", result);
+	printf("Result %f\n", result);
+	
+	printf("Please enter a percentage from which you wanna find full number ");
+	scanf("%d", &perc);
+	printf("Please enter a number of given percentage ");
+	scanf("%d", &number);
+	
+	result = 100 / (perc / number);
+	printf("Result %f \n", result);
 	return 0;
 }
