1714c3aa5b30805588dd7e8b5672ff0209329784
Stepan-Savchuk <stepan_savchuk@protonmail.com>
2023-03-25 10:15:10 +0000

    Tested programs of computing compound interest

--- ci.c
+++ ci.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 
 int main(){
-	double principal = 4200.0;
+	double principal;
 
-	double rate = 12.5;
+	printf("Enter initial principal ");
+	scanf("%f", &principal);
 
-	double n = 2;
+	double rate;
+	printf("Enter a rate ");
+        scanf("%f", &rate);
+
+	double n;
+	printf("Enter amount of payments ");
+        scanf("%f", &n);
+
+	double fr1 = principal*rate * pow( (1 + rate), n);
+	if( fr1 == 0.0) printf("\n ERROR! FR1! \n");
+
+	double fr2 = pow( (1 + rate), n) - 1;
+	if( fr2 == 0.0) printf("\n ERROR! FR2! \n");
 	
-	double amount = (principal * rate * pow( (1+rate), n ) ) / ( pow( (1 + rate), n )-1);
+	double amount = fr1/fr2;
+	if( (amount*0.0) != 0.0) printf("\n ERROR! \n");
 	
 	printf("%f", amount);
 	return 0;
---
+++ ci_i.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+        double principal;
+	printf("Enter initial principal ");
+        scanf("%f", &principal);
+
+        int rate;
+	printf("Enter a rate ");
+        scanf("%d", &rate);
+
+        int n;
+	printf("Enter amount of payments per year ");
+        scanf("%d", &n);
+
+	int years;
+	printf("Enter amount of years ");
+        scanf("%d", &years);
+
+        double amount = principal * pow( (double) (1 + rate/n), (double) (n*years) );
+
+        printf("%f", amount);
+        return 0;
+}
