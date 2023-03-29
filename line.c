0c186cc903b1578c1d959dda4d031968cda14285
Stepan-Savchuk <stepan_savchuk@protonmail.com>
2023-03-29 19:24:42 +0000

    I am working on drawing of circle in terminal

--- line.c
+++ line.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include <curses.h>
 
 typedef struct Point {
@@ -5,11 +7,67 @@ typedef struct Point {
 	int y;
 } Point;
 
+Point* newPoint(int nx, int ny){
+	Point* p = malloc(sizeof(Point));
+	p->x = nx;
+	p->y = ny;
+	return p;
+}
+
+
 typedef struct Line {
 	Point p1;
 	Point p2;
 } Line;
 
+
+typedef struct Circle {
+	Point center;
+	int radius;
+} Circle;
+
+Circle* newCircle(Point p, int r){
+	Circle* c = malloc(sizeof(Circle));
+	c->center = p;
+	c->radius = r;
+	return c;
+}
+
+void drawCircle(Circle circle){
+	Point c = circle.center;
+	int r = circle.radius;
+	move(c.x+r, c.y);
+	insch('*');
+	move(c.x-r, c.y);
+	insch('*');
+	move(c.x, c.y+r*2);
+	insch('*');
+	move(c.x, c.y-r*2);
+	insch('*');
+
+	refresh();
+}
+
 int main(){
+	WINDOW* window;
+	
+	window = initscr();
+	
+	cbreak();
+	noecho();
+	
+	clear();
+	refresh();
+
+	Point* point = newPoint(15, 30);
+
+	Circle* circle = newCircle(*point, 5);
+
+	drawCircle(*circle);
+
+	for(;;){
+		if(getch()=='q') break;
+	}
+	
 	return 0;
 }
